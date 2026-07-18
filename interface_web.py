78d0ad81c70e21141661bc3cee2026e0f6d282af
Luis Gabriel S Novaes <101535096+Lugabriel2022@users.noreply.github.com>
2026-07-18 19:22:56 +0000

    alteraçoes de layout e funcionalidade

--- interface_web.py
+++ interface_web.py
@@ -1,13 +1,28 @@
 from nicegui import ui
 import Instrumentação as it
+from os import system
 
+
+
+system("clear")
 def reset_table():
     tabela.rows = []
-    range = []
+
+def zerar():
+    resultado = 0 
+    display.set_text(resultado)
+    print(resultado)
+
+
+def calcular_sp():
+    resultado = it.calcular_span(float(valor_maior.value), float(valor_menor.value))
+    display.set_text(resultado)
+    print(resultado)
 
 def atualizar_tabela():
     reset_table()
     novas_linhas = []
+    range = []
     maximo = float(value_a.value)
     minimo = float(value_b.value)
     pontos = [4, 8, 12, 16, 20]
@@ -21,26 +36,41 @@ def atualizar_tabela():
     print(range)
     ui.notify("Tabela atualizada!", type='positive')
 
-ui.button(text="set", on_click= atualizar_tabela)
+with ui.row():
+    ui.label("Tabelas Range:")
+    with ui.card().classes('bg-black text-white'):
+        ui.label("Tabela Range")
+        with ui.row():
+            ui.button(text="set", on_click= atualizar_tabela)
+            ui.button(text= "reset", on_click= reset_table)
+        columns = [
+            {'name': "Porcentagem(%)", 'label': "Porcentagem(%)", 'field': 'porcentagem','required': True, 'align': 'center'},
+            {'name': "Valor(mmH2O)", 'label': "Valor(mmH2O)", "field":'valor','required': True, 'align': 'center'},
+            {'name': "Corrente(ma)", 'label': "Corrente(ma)", "field":'corrente','required': True, 'align': 'center'}
+        ]
+        with ui.row():
+            value_a = ui.input(label="Upper Range Limit", value=0).classes('bg-zinc-600 text-white px-3 rounded')
+            value_b = ui.input(label="Lower Range Limit", value=0).classes('bg-zinc-600 text-white px-3 rounded')
 
+        tabela = ui.table(columns=columns, rows=[], row_key="corrente").props("dark")
 
-columns = [
-    {'name': "Porcentagem(%)", 'label': "Porcentagem(%)", 'field': 'porcentagem','required': True, 'align': 'center'},
-    {'name': "Valor(mmH2O)", 'label': "Valor(mmH2O)", "field":'valor','required': True, 'align': 'center'},
-    {'name': "Corrente(ma)", 'label': "Corrente(ma)", "field":'corrente','required': True, 'align': 'center'}
-]
+        novas_linhas = []
+        range = []
 
-value_a = ui.input(label="Upper Range Limit", value=0)
-value_b = ui.input(label="Lower Range Limit", value=0)
+    with ui.card().classes('bg-black text-white'):
+        ui.label("Calculo Span")
+        with ui.row():
+            ui.button("Calcular", on_click= calcular_sp)
+            ui.button("Zerar", on_click= zerar)
 
-tabela = ui.table(columns=columns, rows=[], row_key="corrente")
+        with ui.row():
+            valor_maior = ui.input("Valor Maximo", value=0).classes('bg-zinc-600 text-white px-3 rounded')
+            valor_menor = ui.input("Valor Minimo", value=0).classes('bg-zinc-600 text-white px-3 rounded')
 
-novas_linhas = []
-range = []
+        resultado = 0
+        with ui.row():
+            ui.label(text="Resultado: ")
+            display = ui.label(resultado)
 
-
-
-
-ui.button(text= "reset", on_click= reset_table)
 
 ui.run()
