5e804b9c240600f08232ee54acac6b2aedc6240f
Luis Gabriel S Novaes <101535096+Lugabriel2022@users.noreply.github.com>
2026-07-19 03:01:48 +0000

    melhorias de interface

--- interface_web.py
+++ interface_web.py
@@ -25,13 +25,14 @@ def atualizar_tabela():
     range = []
     maximo = float(value_a.value)
     minimo = float(value_b.value)
+    unidade = value_c.value
     pontos = [4, 8, 12, 16, 20]
     porcentagem = [0, 25, 50, 75, 100]
 
     for c, v in zip(pontos, porcentagem):
         valor = it.calcular_ma(maximo, minimo, c)
         range.append(valor)
-        novas_linhas.append({"porcentagem": v, "valor": valor, "corrente": c})
+        novas_linhas.append({"porcentagem": v, "valor": valor, "corrente": c, "uni": unidade})
     tabela.rows = novas_linhas
     print(range)
     ui.notify("Tabela atualizada!", type='positive')
@@ -45,12 +46,14 @@ with ui.row():
             ui.button(text= "reset", on_click= reset_table)
         columns = [
             {'name': "Porcentagem(%)", 'label': "Porcentagem(%)", 'field': 'porcentagem','required': True, 'align': 'center'},
-            {'name': "Valor(mmH2O)", 'label': "Valor(mmH2O)", "field":'valor','required': True, 'align': 'center'},
-            {'name': "Corrente(ma)", 'label': "Corrente(ma)", "field":'corrente','required': True, 'align': 'center'}
+            {'name': "Valor", 'label': "Valor", "field":'valor','required': True, 'align': 'center'},
+            {'name': "Corrente(ma)", 'label': "Corrente(ma)", "field":'corrente','required': True, 'align': 'center'},
+            {'name': "Unidade", 'label': "Unidade", "field":'uni','required': True, 'align': 'center'}
         ]
         with ui.row():
             value_a = ui.input(label="Upper Range Limit", value=0).classes('bg-zinc-600 text-white px-3 rounded')
             value_b = ui.input(label="Lower Range Limit", value=0).classes('bg-zinc-600 text-white px-3 rounded')
+            value_c = ui.input(label="Insira a unidade do processo", value="none").classes('bg-zinc-600 text-white px-3 rounded')
 
         tabela = ui.table(columns=columns, rows=[], row_key="corrente").props("dark")
 
